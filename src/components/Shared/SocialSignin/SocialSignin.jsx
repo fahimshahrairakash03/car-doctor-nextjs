992fdd760091e54cf5cad6bc4458190f7897a0d9
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2025-01-31 01:00:00 -0400

    updated

--- src/components/Shared/SocialSignin/SocialSignin.jsx
+++ src/components/Shared/SocialSignin/SocialSignin.jsx
@@ -7,7 +7,6 @@ const SocialSignin = () => {
       <button className="text-2xl text-green-700">
         {" "}
         <FaGoogle />
-        asdasdasdsaddsad
       </button>
       <button className="text-2xl">
         {" "}
